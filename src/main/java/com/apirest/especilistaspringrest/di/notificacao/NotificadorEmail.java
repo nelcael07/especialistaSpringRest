0df4fcab4f5e499f8612e51b8dea840108d2809f
nelcael07 <47829164+nelcael07@users.noreply.github.com>
2021-08-26 22:54:49 -0400

    feat: Spring Profile

--- src/main/java/com/apirest/especilistaspringrest/di/notificacao/NotificadorEmail.java
+++ src/main/java/com/apirest/especilistaspringrest/di/notificacao/NotificadorEmail.java
@@ -1,14 +1,20 @@
 package com.apirest.especilistaspringrest.di.notificacao;
 
-import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 import com.apirest.especialistaspringrest.di.modelo.Cliente;
 
+//anotação diz que esse arquivo rodara em prod - produção
+@Profile("Prod")
 //anotação personalizada
 @TipoDoNotificador(NivelUrgencia.URGENTE)
 @Component
 public class NotificadorEmail implements Notificador{
+	
+	public NotificadorEmail() {
+		System.out.println("Email Real");
+	}
 	
 	@Override
 	public void notificar(Cliente cliente, String mensagem){
