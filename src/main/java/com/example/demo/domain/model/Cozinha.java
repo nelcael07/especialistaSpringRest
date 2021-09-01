268492972ad54ef1e6c1c6363265d1035ef727d8
nelcael07 <47829164+nelcael07@users.noreply.github.com>
2021-09-01 10:54:47 -0400

    feat: definindo auto incremento do id na entidade.

--- src/main/java/com/example/demo/domain/model/Cozinha.java
+++ src/main/java/com/example/demo/domain/model/Cozinha.java
@@ -4,22 +4,21 @@ import java.util.Objects;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-//diz que essa class é uma entidade que do banco de dados.
+
 @Entity
-//especifica um nome para a tabela no banco de dados, se não tiver nada, como default ele é o nome da class
-@Table(name="tab_cozinhas")
 public class Cozinha {
 	
-	//anotação que destaca o identificador da tabela	
+	
 	@Id
+	//passando a obrigação de geração de id para o persistor dos dados(Banco de dados)	
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
-	//esse atributo vai representar uma coluna, pode colocar um nome ou usar o nome do atributo como defaut, para isso é só não colocar nada.
-	//se eu deixar sem o @Column também, por a classe estar com o entity ele já identifica que todos os atributos são colunas.	
-	@Column(name="nom_cozinha")
 	private String nome;
 
 	public Long getId() {
