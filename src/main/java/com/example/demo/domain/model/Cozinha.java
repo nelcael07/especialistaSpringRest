cbaa0d22c70b9be1506022daaa381ad350411a50
nelcael07 <47829164+nelcael07@users.noreply.github.com>
2021-09-01 23:00:54 -0400

    feat: lombok

--- src/main/java/com/example/demo/domain/model/Cozinha.java
+++ src/main/java/com/example/demo/domain/model/Cozinha.java
@@ -1,61 +1,33 @@
 package com.example.demo.domain.model;
 
 import java.util.Objects;
-
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Table;
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
 
-@Entity
+// gera gets e sets
+//@Getter
+//@Setter
+//@Entity
+//gerando equals e hashCode
+
+//data gera get set e equals e hashcode
+@Data
+//deixo o equals e hash para gerar somente para os que eu deixar explicito por que se deixar pelo data ele gera o hascode com todos os atributos
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class Cozinha {
 	
-	@Id
-	//passando a obrigação de geração de id para o persistor dos dados(Banco de dados)	
+	//to incluinod o id no equals e hash code	
+	@EqualsAndHashCode.Include
+	@Id	
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
 	private String nome;
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getNome() {
-		return nome;
-	}
-
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-
-	@Override
-	public int hashCode() {
-		return Objects.hash(id);
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Cozinha other = (Cozinha) obj;
-		return Objects.equals(id, other.id);
-	}
-
-	
-	
-	
-	
-	
 }
