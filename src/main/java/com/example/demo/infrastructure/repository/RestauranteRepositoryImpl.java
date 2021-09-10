174e85c5ca3788594da30e2a930b488e68c0f3a9
nelcael07 <47829164+nelcael07@users.noreply.github.com>
2021-09-09 23:41:44 -0400

    feat: implementação de consulta dinamica com JPQL.

--- src/main/java/com/example/demo/infrastructure/repository/RestauranteRepositoryImpl.java
+++ src/main/java/com/example/demo/infrastructure/repository/RestauranteRepositoryImpl.java
@@ -1,10 +1,16 @@
 package com.example.demo.infrastructure.repository;
 
 import java.math.BigDecimal;
+import java.util.HashMap;
 import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
+
 import com.example.demo.domain.model.Restaurante;
 import com.example.demo.respository.queries.RestauranteRespositoryQueries;
 
@@ -18,11 +24,33 @@ public class RestauranteRepositoryImpl implements RestauranteRespositoryQueries 
 	
 	@Override
 	public List<Restaurante> find(String nome, BigDecimal taxaFreteInicial, BigDecimal taxaFreteFinal){
-		var jpql = "from Restaurante where nome like :nome "
-				+ "and taxaFrete between :taxaFreteInicial and :taxaFreteFinal";
-		return manager.createQuery(jpql, Restaurante.class).setParameter("nome", "%"+nome+"%" )
-				.setParameter("taxaFreteInicial", taxaFreteInicial)
-				.setParameter("taxaFreteFinal", taxaFreteFinal)
-				.getResultList();
+		var jpql = new StringBuilder();
+		jpql.append("from Restaurante where 0 = 0 ");
+		
+		var parametros = new HashMap<String, Object>();
+		
+		if (StringUtils.hasLength(nome)) {
+			jpql.append("and nome like:nome ");
+			parametros.put("nome", "%"+nome+"%");
+		}
+		
+		if(taxaFreteInicial != null) {
+			jpql.append("and taxaFrete >= :taxaFreteInicial ");
+			parametros.put("taxaFreteInicial", taxaFreteInicial);
+		}
+		
+		if (taxaFreteFinal != null) {
+			jpql.append("and taxaFrete <= :taxaFreteFinal");
+			parametros.put("taxaFreteFinal", taxaFreteFinal);
+		}
+		
+		//to string para converter para string pois o createquey recebe um string e não uma StringBuilder		
+		TypedQuery<Restaurante> query = manager.createQuery(jpql.toString(), Restaurante.class);
+				
+		parametros.forEach((chave, valor)->{
+			query.setParameter(chave, valor);
+		});
+		
+		return query.getResultList();
 	}
 }
