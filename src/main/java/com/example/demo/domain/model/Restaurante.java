64ea82b883d72c8ba6db9bed4ade7f65c586ea47
nelcael07 <47829164+nelcael07@users.noreply.github.com>
2021-09-13 14:10:39 -0400

    feat: implementando classes incorporaveis  com @Embedded @Embeddable

--- src/main/java/com/example/demo/domain/model/Restaurante.java
+++ src/main/java/com/example/demo/domain/model/Restaurante.java
@@ -4,6 +4,8 @@ import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -35,6 +37,11 @@ public class Restaurante {
 	@ManyToOne	
 	@JoinColumn( nullable = false )
 	private Cozinha cozinha;
+	
+	// essa propriedade  é uma propriedade compartilhada, um @Embeddable, é de um tipo incorporado.
+	@Embedded
+	private Endereco endereco;
+	
 	
 	@JsonIgnore
 	@ManyToMany
