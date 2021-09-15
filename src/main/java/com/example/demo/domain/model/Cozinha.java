3f5f035284faabf7f8c6d070381ac64d67a7ef79
nelcael07 <47829164+nelcael07@users.noreply.github.com>
2021-09-15 07:34:21 -0400

    feat: generation não é uma boa pratica em produção.

--- src/main/java/com/example/demo/domain/model/Cozinha.java
+++ src/main/java/com/example/demo/domain/model/Cozinha.java
@@ -1,11 +1,16 @@
 package com.example.demo.domain.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonRootName;
 
 import lombok.Data;
@@ -24,5 +29,11 @@ public class Cozinha {
 	
 	@Column(nullable = false)
 	private String nome;
+	
+	private String descricao;
+	
+	@JsonIgnore
+	@OneToMany(mappedBy = "cozinha")
+	private List<Restaurante> restaurantes = new ArrayList<>();
 	
 }
