1fb879f7145080a638b68096fe093c545faf870a
nelcael07 <47829164+nelcael07@users.noreply.github.com>
2021-09-16 12:47:27 -0400

    feat: adicionando dados de teste por callback

--- src/main/java/com/example/demo/domain/repository/RestauranteRepository.java
+++ src/main/java/com/example/demo/domain/repository/RestauranteRepository.java
@@ -12,22 +12,14 @@ import org.springframework.data.jpa.repository.Query;
 import com.example.demo.domain.model.Restaurante;
 import com.example.demo.infrastructure.respository.queries.RestauranteRespositoryQueries;
 
-//implementar JpaspecificationExecutor faz com que possa ser utilzado spec nessa classe.
 @Primary
 public interface RestauranteRepository extends CustomJpaRepository<Restaurante, Long>, RestauranteRespositoryQueries, 
 				JpaSpecificationExecutor<Restaurante>{
 	
-	//fazendo join de cozinha junto com a consulta de restaurante, ou seja, ele faz uma consulta só.
-	//o left join fetch faz o join funcionar com o final tomany.
-	//isso ocorre por que no tomany o padrão é lazy.
-	
-	//O PROBLEMA DESSA CONSULTA É QUE ELA GERA MAIS RESTAURANTES NO JSON.
-	@Query("from Restaurante r join fetch r.cozinha left join fetch r.formasPagamento")
+	@Query("from Restaurante r join fetch r.cozinha ")
 	List<Restaurante> findAll();
 	
 	List<Restaurante> getByTaxaFreteBetween(BigDecimal taxaInicial, BigDecimal taxaFinal);
-
-//	List<Restaurante> consultarPorNome(String nome, Long id);
 
 	Optional<Restaurante> getFirstByNomeContaining(String nome);
 
