e0981d6a5a4b612ae40f4b4a25c3a439ab14da36
nelcael07 <47829164+nelcael07@users.noreply.github.com>
2021-09-22 22:26:19 -0400

    feat: Implementando a customização do ResponseEntityExeptionHandler.

--- src/main/java/com/example/demo/api/controller/CidadeController.java
+++ src/main/java/com/example/demo/api/controller/CidadeController.java
@@ -1,13 +1,10 @@
 package com.example.demo.api.controller;
 
-import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,13 +16,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.demo.api.exceptionhandler.Problema;
-import com.example.demo.domain.exception.CidadeNaoEncontradoException;
-import com.example.demo.domain.exception.EntidadeNaoEncontradaException;
 import com.example.demo.domain.exception.EstadoNaoEncontradoException;
 import com.example.demo.domain.exception.NegocioException;
 import com.example.demo.domain.model.Cidade;
-import com.example.demo.domain.model.Estado;
 import com.example.demo.domain.repository.CidadeRepository;
 import com.example.demo.domain.service.CadastroCidadeService;
 
@@ -75,6 +68,5 @@ public class CidadeController {
 	public void remover(@PathVariable Long id) {
 		cidadeService.remover(id);
 	}
-	
 	
 }
--- src/main/java/com/example/demo/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/example/demo/api/exceptionhandler/ApiExceptionHandler.java
@@ -2,11 +2,12 @@ package com.example.demo.api.exceptionhandler;
 
 import java.time.LocalDateTime;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.HttpMediaTypeNotSupportedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import com.example.demo.domain.exception.EntidadeEmUsoException;
@@ -18,35 +19,39 @@ import com.example.demo.domain.exception.NegocioException;
 public class ApiExceptionHandler extends ResponseEntityExceptionHandler{
   
 	@ExceptionHandler(EntidadeNaoEncontradaException.class)
-	public ResponseEntity<?> handleEstadoNaoEncontradoException(EntidadeNaoEncontradaException e){
-		Problema problema = Problema.builder()
-				.dataHora(LocalDateTime.now())
-				.Mensagem(e.getMessage())
-				.build();
-		
-		return ResponseEntity.status(HttpStatus.NOT_FOUND)
-				.body(problema);
+	public ResponseEntity<?> handleEstadoNaoEncontradoException(EntidadeNaoEncontradaException e, WebRequest request){
+		return handleExceptionInternal(e, e.getMessage(), new HttpHeaders(), HttpStatus.NOT_FOUND, request);
 	}
 	
 	@ExceptionHandler(NegocioException.class)
-	public ResponseEntity<?> handleNegocioException(NegocioException e ){
- 		Problema problema = Problema.builder()
-				.dataHora(LocalDateTime.now())
-				.Mensagem(e.getMessage())
-				.build();
-		
-		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(problema);
+	public ResponseEntity<?> handleNegocioException(NegocioException e,  WebRequest request ){
+		return handleExceptionInternal(e, e.getMessage(), new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
 	}
 	
-
 	@ExceptionHandler(EntidadeEmUsoException.class)
-	public ResponseEntity<?> handleEntidadeEmUsoException(EntidadeEmUsoException e){
-		Problema problema = Problema.builder()
-				.dataHora(LocalDateTime.now())
-				.Mensagem(e.getMessage())
-				.build();
-		return ResponseEntity.status(HttpStatus.CONFLICT).body(problema);
+	public ResponseEntity<?> handleEntidadeEmUsoException(EntidadeEmUsoException e,  WebRequest request){
+		return handleExceptionInternal(e, e.getMessage(), new HttpHeaders(), HttpStatus.CONFLICT, request);
 	}
 	
+	@Override
+	protected ResponseEntity<Object> handleExceptionInternal(Exception ex, Object body, HttpHeaders headers,
+			HttpStatus status, WebRequest request) {
+		
+		if (body == null) {
+			body = Problema.builder()
+					.dataHora(LocalDateTime.now())
+					//getReasonPhrase pega do status uma breve descrição.				
+					.Mensagem(status.getReasonPhrase())
+					.build();
+		} else if(body instanceof String) {
+			body = Problema.builder()
+					.dataHora(LocalDateTime.now())
+					//fazendo um cash de objeto para String					
+					.Mensagem((String) body)
+					.build();
+		}
+		
+		return super.handleExceptionInternal(ex, body, headers, status, request);
+	}
 	
 }
