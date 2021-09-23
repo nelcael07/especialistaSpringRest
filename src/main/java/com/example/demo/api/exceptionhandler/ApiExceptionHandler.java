b70851891d59e0f250731fabd0a28d1c6ee1cb9d
nelcael07 <47829164+nelcael07@users.noreply.github.com>
2021-09-22 21:53:03 -0400

    feat: implementação do extends ResponseEntityExceptionHandler em ApiExceptionHandler.

--- src/main/java/com/example/demo/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/example/demo/api/exceptionhandler/ApiExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.HttpMediaTypeNotSupportedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import com.example.demo.domain.exception.EntidadeEmUsoException;
 import com.example.demo.domain.exception.EntidadeNaoEncontradaException;
@@ -14,7 +15,7 @@ import com.example.demo.domain.exception.NegocioException;
 
 //anotação diz que essa classe vai ser a que vai captar as exceções de todos os controllers para retornar EntityManager aqui.
 @ControllerAdvice
-public class ApiExceptionHandler {
+public class ApiExceptionHandler extends ResponseEntityExceptionHandler{
   
 	@ExceptionHandler(EntidadeNaoEncontradaException.class)
 	public ResponseEntity<?> handleEstadoNaoEncontradoException(EntidadeNaoEncontradaException e){
@@ -35,16 +36,6 @@ public class ApiExceptionHandler {
 				.build();
 		
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(problema);
-	}
-	
-	//exception de midia(solicitar xml e só suportar json)	
-	@ExceptionHandler(HttpMediaTypeNotSupportedException.class)
-	public ResponseEntity<?> handleHttpMediaTypeNotSupportedException( ){
-		Problema problema = Problema.builder()
-				.dataHora(LocalDateTime.now())
-				.Mensagem("O tipo de midia não é aceito.")
-				.build();
-		return ResponseEntity.status(HttpStatus.UNSUPPORTED_MEDIA_TYPE).body(problema); 
 	}
 	
 
