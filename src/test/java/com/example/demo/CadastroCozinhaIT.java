737eb8a9611d54a1842b4ce8975b65f231931426
nelcael07 <47829164+nelcael07@users.noreply.github.com>
2021-10-01 15:06:54 -0400

    feat: implementando o primeiro test de api utilizando o rest-assured.

--- src/test/java/com/example/demo/CadastroCozinhaIT.java
+++ src/test/java/com/example/demo/CadastroCozinhaIT.java
@@ -1,12 +1,31 @@
 package com.example.demo;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
 
-@SpringBootTest
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+
+//levanta uma aplicação em porta aleatoria para conseguir fazer os teste
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class CadastroCozinhaIT {
 	
+	//pega a porta utilizada para subir a aplicação		
+	@LocalServerPort
+	private int port; 
+	
+	@Test
 	public void testarDeveRetornarStatus200_QuandoConsultarCozinha() {
-		 
+		RestAssured.given()
+			.basePath("/cozinhas")
+			.port(port)
+			.accept(ContentType.JSON)
+		.when()
+			.get()
+		.then()
+			.statusCode(200);
+		
 	}
 	
 }
